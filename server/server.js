43ee7167d591cc2568274a68c6556bd55a5e772a
RockingMotog <rockingmotog@gmail.com>
2022-08-13 07:17:19 +0530

    Get Records by ID, Get all Records

--- server/server.js
+++ server/server.js
@@ -3,16 +3,16 @@ const cors = require('cors');
 require('dotenv').config({ path: './config.env' });
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.SERVERPORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cors());
-//app.use(require('./routes/record'));
+app.use(require('./routes/record'));
 
 const dbo = require('./db/conn.js');
 
 app.listen(port, () => {
-    dbo.connectToServer()
+    dbo.connectToServer(function (err) { if (err) console.error(err); })
     console.log(`App is running on the Port ${port}`);
 })
 
