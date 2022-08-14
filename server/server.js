1991e8a0eaefbe6df89a250289978f1d713cc70c
RockingMotog <rockingmotog@gmail.com>
2022-08-14 23:49:48 +0530

    User Authentication

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser'); 
+const jwt  = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const User = require('./model/users')
+
 require('dotenv').config({ path: './config.env' });
 
 
 const port = process.env.SERVERPORT || 5000;
 const app = express();
+const urlencodedParser = bodyParser.urlencoded({extended: false})
+app.use(bodyParser.json(), urlencodedParser)
 app.use(express.json());
 app.use(cors());
 app.use(require('./routes/records.js'));
