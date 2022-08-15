0bf04e242d07bcde3fb519618ac6bfbe3285785b
rockingmotog <rockingmotog@gmail.com>
2022-08-15 09:20:31 -0700

    mongoose

--- server/server.js
+++ server/server.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser'); 
-const jwt  = require('jsonwebtoken')
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
-const User = require('./model/users')
+const User = require('./models/user.js');
+const { MongoTopologyClosedError } = require('mongodb');
+const { default: mongoose } = require('mongoose');
 
 require('dotenv').config({ path: './config.env' });
 
 
 const port = process.env.SERVERPORT || 5000;
 const app = express();
-const urlencodedParser = bodyParser.urlencoded({extended: false})
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use(bodyParser.json(), urlencodedParser)
 app.use(express.json());
 app.use(cors());
 app.use(require('./routes/records.js'));
 
-const dbo = require('./db/conn.js');
+mongoose.connect(ATLAS_URI, { useNewUrlParser: true, useUnifiedTopolog: true })
+    .then((res) => {
 
-app.listen(port, () => {
-    dbo.connectToServer(function (err) { if (err) console.error(err); })
-    console.log(`App is running on the Port ${port}`);
-})
+        app.listen(port, () => { console.log(`App is running on the Port ${port}`) })
+    })
+    .catch(err => console.log(err))
+
 
 
 
