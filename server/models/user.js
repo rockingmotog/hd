c9b8223dbf3a6c32fb06095f0af22f122ddfdad7
RockingMotog <rockingmotog@gmail.com>
2022-08-16 18:34:46 +0530

    dbUser

--- server/models/user.js
+++ server/models/user.js
@@ -17,5 +17,4 @@ const userSchema = mongoose.Schema({
 }, { timestamps: true })
 
 const User = mongoose.model('User', userSchema)
-
 module.exports = User; 
--- server/server.js
+++ server/server.js
@@ -24,3 +24,45 @@ mongoose.connect(dbUri, { useUnifiedTopology: true, useNewUrlParser: true })
         })
     })
     .catch(err => { console.log(err) })
+
+// Validate the if the user details already exists & Register the user 
+
+
+app.post('/register', async (req, res) => {
+    const user = req.body;
+
+    const givenUsername = await User.findOne({ username: user.username })
+    const takenEmail = await User.findOne({ email: user.email })
+
+    if (givenUsername || takenEmail) {
+        console.log('Username or Email alredy taken by someone else')
+    } else {
+        user.password = await bcrypt.hash(req.body.password, 10);
+        const dbUser = new User({
+            username: user.username.toLowerCase(),
+            email: user.email.toLowerCase(),
+            password: user.password
+        })
+        dbUser.save();
+        res.json({ message: "Success !! " })
+    }
+
+})
+
+app.post('/login', async (req, res) => {
+    const userLogin = req.body;
+    User.findOne({ username: userLogin.username })
+        .then(dbUser => {
+            if (!dbUser) {
+                return res.json({
+                    message: "Invaild Username & Password"
+                })
+            }
+            bcrypt.compare(userLogin.password, dbUser.password)
+            .then(isCorrect){ 
+                const payload = 
+            }
+        })
+
+}
+)
